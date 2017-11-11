ac8b262c69e1f9114ea91269bddbb962ece2f038
GavinThomas1192 <gthomas1192@gmail.com>
2017-11-11 15:03:29 -0800

    first

---
+++ popup.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let listJobs = document.getElementById('openJobs');
+    let div = document.getElementById('div');
+
+    listJobs.addEventListener('click', function () {
+
+        div.innerHTML = 'hello'
+
+
+    })
+
+});
+
+
